3cdbb9385db899b51341a6efdb6e38c464dd9ef1
Ammu1492 <60737792+Ammu1492@users.noreply.github.com>
2023-01-24 22:39:41 +0530

    Added a sample

---
+++ ViewModel/DataViewModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MauiExample
+{
+    public class DataViewModel
+    {
+        public ObservableCollection<DataModel> SocialMedia {  get; set; }
+        public DataViewModel() {
+            SocialMedia = new ObservableCollection<DataModel> { 
+                new DataModel() { Name = "Facebook", ID = 0 },
+                new DataModel() { Name = "Telegram", ID = 1 },
+                new DataModel() { Name = "YouTube", ID = 2 },
+                new DataModel() { Name = "Instagram", ID = 3 },
+                new DataModel() { Name = "Twitter", ID = 4 },
+                new DataModel() { Name = "Google Plus", ID = 5 },
+                new DataModel() { Name = "WhatsApp", ID = 6 },
+                new DataModel() { Name = "Televzr", ID = 7 },
+                new DataModel() { Name = "Tik Tok", ID = 8 }
+            };
+        }
+    }
+}
